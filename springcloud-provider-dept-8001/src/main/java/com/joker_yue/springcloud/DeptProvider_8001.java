cf6587460e79ac5ca1d2d10bfdae949a6a32974c
JokerYue <Joker_Yue@qq.com>
2023-08-16 17:19:44 +0800

    将提供者注册进Eureka

--- springcloud-provider-dept-8001/src/main/java/com/joker_yue/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/joker_yue/springcloud/DeptProvider_8001.java
@@ -2,6 +2,7 @@ package com.joker_yue.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Joker
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 // 启动类
 @SpringBootApplication
+@EnableEurekaClient // 在服务启动后自动将服务端注册到Eureka
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
