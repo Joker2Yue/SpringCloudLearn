5af34e22de939071a06d17021c5278721953b0d3
JokerYue <Joker_Yue@qq.com>
2023-08-16 14:19:26 +0800

    测试数据库连接与url访问

---
+++ springcloud-provider-dept-8001/src/main/java/com/joker_yue/springcloud/DeptProvider_8001.java
@@ -0,0 +1,17 @@
+package com.joker_yue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joker
+ * @version 1.0
+ * @date 2023/8/15 21:00
+ */
+// 启动类
+@SpringBootApplication
+public class DeptProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8001.class, args);
+    }
+}
---
+++ springcloud-provider-dept-8001/src/main/java/com/joker_yue/springcloud/controller/DeptController.java
@@ -0,0 +1,39 @@
+package com.joker_yue.springcloud.controller;
+
+import com.joker_yue.springcloud.pojo.Dept;
+import com.joker_yue.springcloud.service.DeptService;
+import com.joker_yue.springcloud.service.DeptServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 提供Restful服务
+ * @author Joker
+ * @version 1.0
+ * @date 2023/8/15 20:12
+ */
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService deptService;
+
+    @PostMapping("/dept/add")
+    public boolean addDept(Dept dept){
+        return deptService.addDept(dept);
+    }
+
+    @PostMapping("/dept/get/{id}")
+    public Dept getDept(@PathVariable("id") Long id){
+        return deptService.queryById(id);
+    }
+
+    @GetMapping("/dept/list")
+    public List<Dept> list(){
+        return deptService.queryAll();
+    }
+}
