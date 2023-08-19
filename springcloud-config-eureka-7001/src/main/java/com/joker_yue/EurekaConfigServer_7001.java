0fdc8fe34791411a029216d91e3b01602d9e4b64
JokerYue <Joker_Yue@qq.com>
2023-08-19 19:59:28 +0800

    springcloud-config-eureka-7001 远程配置注册中心

--- springcloud-config-eureka-7001/src/main/java/com/joker_yue/EurekaConfigServer_7001.java
+++ springcloud-config-eureka-7001/src/main/java/com/joker_yue/EurekaConfigServer_7001.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer // 服务端的启动类，可以接收别人注册进来
-public class EurekaServer_7001 {
+public class EurekaConfigServer_7001 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer_7001.class,args);
+        SpringApplication.run(EurekaConfigServer_7001.class,args);
     }
 }
