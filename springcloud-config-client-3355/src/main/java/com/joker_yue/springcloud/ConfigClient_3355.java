662fb33fca776ffdbd2c191d2ae36a42c4312354
JokerYue <Joker_Yue@qq.com>
2023-08-19 19:21:35 +0800

    springcloud-config-client-3355 配置

---
+++ springcloud-config-client-3355/src/main/java/com/joker_yue/springcloud/ConfigClient_3355.java
@@ -0,0 +1,16 @@
+package com.joker_yue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joker
+ * @version 1.0
+ * @date 2023/8/19 19:01
+ */
+@SpringBootApplication
+public class ConfigClient_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355.class, args);
+    }
+}
